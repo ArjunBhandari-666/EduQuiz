4155d673ca80015c9794fe01081a87dc37ab77dd
Arjun Bhandari <72136655+ArjunBhandari-666@users.noreply.github.com>
2022-03-15 19:46:09 +0530

    anim

---
+++ Animation.js
@@ -0,0 +1,4 @@
+gsap.from('.QuerryFormBox',  {delay: 2,x:-100,y:100, duration: 5, ease: 'expo.inOut', opacity:0})
+gsap.from('.AboutUsBox',  {delay: 1,x:300, duration: 1, ease: 'expo.inOut', opacity:0})
+gsap.from('.ContactUsBox',  {delay: 1, duration: 1, ease: 'expo.inOut', opacity:0})
+gsap.from('.BottomCircle',  {delay: 1,x:-100, duration: 1, ease: 'expo.inOut', opacity:0})
